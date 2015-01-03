1879fa83f072a73a11bdfa6c03b88986d68119dc
nmkr <tech@ssb-tech.at>
2015-01-03 12:43:20 +0100

    misc updates

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -9,41 +9,25 @@ class UserTableSeeder extends Seeder
 
         User::create(array(
             'anrede'   => 'Herr',
-            'vorname'  => 'Alex',
-            'nachname' => 'M.',
-            'email'    => 'office@nmkr.at',
-            'password' => 'changeme'
-        ));
-
-        User::create(array(
-            'anrede'   => 'Herr',
-            'titel'    => 'Dr.',
-            'vorname'  => 'Max',
-            'nachname' => 'Mustermann',
-            'email'    => 'max@muster.at',
-            'password' => 'changeme'
+            'vorname'  => 'Jon',
+            'nachname' => 'Doe',
+            'email'    => 'admin@admin.app',
+            'password' => 'admin'
         ));
 
         Role::create(array(
             'name' => 'admin'
         ));
-
         Role::create(array(
             'name' => 'member'
         ));
-
         Role::create(array(
             'name' => 'trial'
         ));
 
-        DB::table('role_user')->create(array(
+        DB::table('role_user')->insert(array(
             'role_id' => 1,
             'user_id' => 1
-        ));
-
-        DB::table('role_user')->create(array(
-            'role_id' => 2,
-            'user_id' => 2
         ));
     }
 
